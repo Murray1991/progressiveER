b6b7af868e31c8a5168a50ee2d26d726c01784cc
Leonardo Gazzarri <leonardo.gazzarri@ipvs.uni-stuttgart.de>
2020-11-22 16:15:58 +0100

    minor

---
+++ ProgressiveBlocking/run_dbpedia.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASE=/home/ubuntu/parallel-entity-resolution-framework/data/cleanCleanErDatasets/
+D1=DBPedia1Profiles
+D2=DBPedia2Profiles
+GT=DBPediaIdDuplicates
+FRACTION=1
+
+
+
+mvn clean package
+#java -cp target/progressiveER-1.0-jar-with-dependencies.jar Experiments.complete_experiment.JedaiCepExperiments_complete /Users/leonardo/Desktop/data/cleanCleanErDatasets/ imdbProfiles dbpediaProfiles moviesIdDuplicates 1
+#java -cp target/progressiveER-1.0-jar-with-dependencies.jar Experiments.complete_experiment.JedaiCepExperiments_complete $BASE imdbProfiles dbpediaProfiles moviesIdDuplicates 1
+
+java -cp target/progressiveER-1.0.jar Experiments.complete_experiment.JedaiCepExperiments_complete $BASE $D1 $D2 $GT $FRACTION
